a006057f8dace9b99475872eab26c7e6bcdc8637
Haena <johanna0123@naver.com>
2023-07-17 17:08:35 +0900

    프로젝트명 수정, build.gradle dependencies 및 application.properties 추가

--- src/test/java/com/sparta/followfollowmeproject/FollowFollowMeProjectApplicationTests.java
+++ src/test/java/com/sparta/followfollowmeproject/FollowFollowMeProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sparta.followfollomeproject;
+package com.sparta.followfollowmeproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FollowFollomeProjectApplicationTests {
+class FollowFollowMeProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
