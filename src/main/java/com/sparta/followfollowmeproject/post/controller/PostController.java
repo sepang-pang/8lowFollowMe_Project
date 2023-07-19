127dac700d51b64d4f76e213a67a20e12fee456b
jin <132440453+05030522@users.noreply.github.com>
2023-07-19 09:27:18 +0900

    Feature / post & comment like 추가++

--- src/main/java/com/sparta/followfollowmeproject/post/controller/PostController.java
+++ src/main/java/com/sparta/followfollowmeproject/post/controller/PostController.java
@@ -20,7 +20,6 @@ import java.util.List;
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService postService;
-    private final PostLikeService postLikeService;
 
     @GetMapping
     public ResponseEntity<List<PostResponseDto>> getAllPosts() {
