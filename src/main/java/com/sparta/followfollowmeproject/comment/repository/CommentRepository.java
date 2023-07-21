73cb981b0faecc947c2ef03f5ac2cc11acd7c850
Haena <johanna0123@naver.com>
2023-07-21 19:32:16 +0900

    [fix] conflicts 정리

--- src/main/java/com/sparta/followfollowmeproject/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/followfollowmeproject/comment/repository/CommentRepository.java
@@ -7,9 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository <Comment, Long> {
-<<<<<<< HEAD
-	 List<Comment> findAllByPostOrderByCreatedAtDesc(Post post);
-=======
 	List<Comment> findAllByPostOrderByCreatedAtDesc(Post post);
->>>>>>> fad7d056e5a8782b5efe813358dfd2f6b3edfc58
 }
