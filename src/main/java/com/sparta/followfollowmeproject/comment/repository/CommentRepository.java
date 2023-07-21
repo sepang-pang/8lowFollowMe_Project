8a6b0040f49aec336fa512d7b4322a0a30199751
jin <132440453+05030522@users.noreply.github.com>
2023-07-21 20:01:27 +0900

    [fix] comment like Count 수정사항 반영

--- src/main/java/com/sparta/followfollowmeproject/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/followfollowmeproject/comment/repository/CommentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository <Comment, Long> {
+    List<Comment> findAllByPostOrderByCreatedAtDesc(Post post);
 }
