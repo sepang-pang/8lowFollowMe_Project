feed199f8b38fe193024387f4a0482d78efda4b0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-07-23 10:56:05 +0900

    <feat> : profile 기능 구현

---
+++ src/main/java/com/sparta/followfollowmeproject/profile/entity/Profile.java
@@ -0,0 +1,61 @@
+package com.sparta.followfollowmeproject.profile.entity;
+
+import com.sparta.followfollowmeproject.profile.dto.ProfileRequestDto;
+import com.sparta.followfollowmeproject.user.entity.Timestamped;
+import com.sparta.followfollowmeproject.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "profile")
+@NoArgsConstructor
+public class Profile extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "nickName")
+    private String nickName;
+
+    @Column(name = "intro")
+    private String intro;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "photo")
+    private String photoUrl;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Profile(ProfileRequestDto profileRequestDto, User user) {
+        this.user = user;
+        this.username = user.getUsername();
+        this.nickName = profileRequestDto.getNickName();
+        this.intro = profileRequestDto.getIntro();
+        this.age = profileRequestDto.getAge();
+        this.photoUrl = profileRequestDto.getMediaUrl();
+    }
+
+    public void update(ProfileRequestDto profileRequestDto) {
+
+        this.nickName = profileRequestDto.getNickName();
+        this.intro = profileRequestDto.getIntro();
+        this.age = profileRequestDto.getAge();
+        this.photoUrl = profileRequestDto.getMediaUrl();
+
+    }
+}
